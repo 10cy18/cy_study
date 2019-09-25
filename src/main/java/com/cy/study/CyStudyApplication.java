1f759898066471352acf891627363be556837eba
10cy18 <cy986275730@qq.com>
2019-09-25 14:23:45 +0800

    Initial commit

---
+++ src/main/java/com/cy/study/CyStudyApplication.java
@@ -0,0 +1,13 @@
+package com.cy.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CyStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CyStudyApplication.class, args);
+    }
+
+}
